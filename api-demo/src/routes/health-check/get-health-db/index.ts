ae0cbdce76f4864a1d2e47163c0c11d4dcdd0215
justinl-y <jlevettyeats@semios.com>
2026-04-28 13:44:54 -0700

    Code review changes

--- api-demo/src/routes/health-check/get-health-db/index.ts
+++ api-demo/src/routes/health-check/get-health-db/index.ts
@@ -2,10 +2,10 @@ import { checkDb } from '#services/health/health.service';
 
 import type { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify';
 
-async function getHealthDB(this: FastifyInstance, request: FastifyRequest, reply: FastifyReply) {
+async function getHealthDB(this: FastifyInstance, _request: FastifyRequest, reply: FastifyReply) {
   const result = await checkDb(this.db);
 
-  reply.status(200).send(result);
+  reply.send(result);
 }
 
 export default getHealthDB;
--- api-demo/src/routes/health-check/get-health-eb/index.ts
+++ api-demo/src/routes/health-check/get-health-eb/index.ts
@@ -2,7 +2,7 @@ import { checkEb } from '#services/health/health.service';
 
 import type { FastifyRequest, FastifyReply } from 'fastify';
 
-function getHealthEB(request: FastifyRequest, reply: FastifyReply) {
+function getHealthEB(_request: FastifyRequest, reply: FastifyReply) {
   reply.send(checkEb());
 }
 
