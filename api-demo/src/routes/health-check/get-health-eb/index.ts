444d63f71ef85379d9e05699b05831c19850515a
Justin LY <justin@sumofparts.ca>
2026-05-04 10:59:59 -0700

    #29 - (fix/get-health-eb)

--- api-demo/src/routes/health-check/get-health-eb/index.ts
+++ api-demo/src/routes/health-check/get-health-eb/index.ts
@@ -3,7 +3,7 @@ import { checkEb } from '#services/health/health.service';
 import type { FastifyRequest, FastifyReply } from 'fastify';
 
 function getHealthEB(_request: FastifyRequest, reply: FastifyReply) {
-  return reply.send(checkEb());
+  reply.send(checkEb());
 }
 
 export default getHealthEB;
